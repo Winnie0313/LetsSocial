8f17cdc5cd053bec27496f27ef8fd9afde7fd249
Winnie0313 <liwanyi0313@gmail.com>
2022-10-25 23:08:00 -0700

    add styled components

--- client/my-react-app/src/components/Navbar.jsx
+++ client/my-react-app/src/components/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 60px;
+  background-color: #1d3557;
+  color: white
+`
+const Wrapper = styled.div`
+  padding: 10px 20px;
+`
+const Left = styled.div``
+const Middle = styled.div``
+const Right = styled.div``
 
 const Navbar = () => {
   return (
-    <div>
-      navbar
-    </div>
+    <Container>
+      <Wrapper>
+        <Left></Left>
+        <Middle></Middle>
+        <Right></Right>
+      </Wrapper>
+    </Container>
   )
 }
 
